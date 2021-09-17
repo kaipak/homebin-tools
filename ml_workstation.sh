e7ff2f5ff7d5fffb07de36ffc702540167de0691
Kai Pak <kai@kaipak.org>
2021-09-16 17:49:57 -0700

    Directory update for tensorboard.

--- ml_workstation.sh
+++ ml_workstation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run services for ML development.
 cd ~/dev && nohup jupyter lab --no-browser --port 1234 &
-cd ~/models && nohup tensorboard --logdir runs &
+cd ~/models && nohup tensorboard --logdir ~/models/runs &
