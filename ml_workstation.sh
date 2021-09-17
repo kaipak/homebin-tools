1e3a55cf9e71d12d7132e1bde03a56c7c7a7a7b8
Kai Pak <kaipak@jpl.nasa.gov>
2021-09-17 11:31:42 -0700

    Add getopts to script to specify ports.

--- ml_workstation.sh
+++ ml_workstation.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 # Run services for ML development.
-cd ~/dev && nohup jupyter lab --no-browser --port 1234 &
-cd ~/models && nohup tensorboard --logdir ~/models/runs &
+JUPYTER_PORT=8888
+JUPYTER_OUTPUT=~/dev
+TENSORBOARD_PORT=6006
+TENSORBOARD_OUTPUT=~/models/runs
+
+usage() {
+  echo "Usage: ml_workstation [ -j JUPYTER_PORT ] [ -t TENSORBOARD_PORT ]";
+  exit 2;
+}
+
+while getopts ":j:t:" arg; do
+  case ${arg} in
+    j) JUPYTER_PORT=${OPTARG}
+       ;;
+    t) TENSORBOARD_PORT=${OPTARG}
+       ;;
+    *) usage
+       ;;
+  esac
+done
+
+echo "Hello ${TENSORBOARD_PORT}"
+
+cd ~/dev && nohup jupyter lab --no-browser --port ${JUPYTER_PORT} &
+cd ~/models && nohup tensorboard --port ${TENSORBOARD_PORT} --logdir ~/models/runs &
